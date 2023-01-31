38ec160acca4c9fb5c46f2f67a49e826778e4873
Alexey <mametevalex@gmail.com>
2023-01-31 03:27:08 +0300

    [Fixed] Tg link is not working

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app.layout = html.Div([
         500,
         value=300,
         id='hist_density'
-    )
+    ),
     dcc.Link(href='https://t.me/miptphyshub', title="Что тут происходит?")
 ])
 
